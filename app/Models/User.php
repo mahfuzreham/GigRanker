a0779b28239126ec768d6a8960302e3f939643eb
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2026-08-15 22:15:27 +0600

    feat: add user project relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -28,5 +29,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
     }
 }
