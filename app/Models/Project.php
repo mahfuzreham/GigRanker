55312f63e20c233bfeaab1e98abecb43db23c389
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2026-08-15 22:31:36 +0600

    feat: add project site URL attribute

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -14,6 +14,7 @@ class Project extends Model
         'user_id',
         'name',
         'gig_url',
+        'site_url',
         'gig_title',
         'gig_description',
         'service_category',
