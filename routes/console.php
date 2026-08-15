f0c8a8e648cf2d296be0f03069a7397d92b8dc5b
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2026-08-16 05:43:22 +0600

    refactor: keep console routes focused on app commands

--- routes/console.php
+++ routes/console.php
@@ -2,15 +2,8 @@
 
 declare(strict_types=1);
 
-use App\Console\Commands\DeploymentLogCommand;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('gigranker:status', function (): void {
     $this->info('GigRanker application scaffold is ready.');
 })->purpose('Check GigRanker application status');
-
-Artisan::starting(function ($artisan): void {
-    $artisan->resolveCommands([
-        DeploymentLogCommand::class,
-    ]);
-});
