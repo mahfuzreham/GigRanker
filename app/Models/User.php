008a12d4c607182ff5de937d7bfcfc529ef9b7c9
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2026-08-16 01:01:43 +0600

    feat: link users to payment records

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,6 +48,11 @@ class User extends Authenticatable
         return $this->hasMany(Subscription::class);
     }
 
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+
     public function activeSubscription(): HasOne
     {
         return $this->hasOne(Subscription::class)->where('status', 'active')->latestOfMany();
