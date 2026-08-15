ac7be31189c2228a97e81be0fb191be3aca5ed11
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2026-08-16 01:17:11 +0600

    feat: add country targeting markets

---
+++ config/markets.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'countries' => [
+        'US' => ['name' => 'United States', 'currency' => 'USD', 'locale' => 'en_US', 'direction' => 'ltr'],
+        'GB' => ['name' => 'United Kingdom', 'currency' => 'GBP', 'locale' => 'en_GB', 'direction' => 'ltr'],
+        'EU' => ['name' => 'European Union', 'currency' => 'EUR', 'locale' => 'en_EU', 'direction' => 'ltr'],
+        'BD' => ['name' => 'Bangladesh', 'currency' => 'BDT', 'locale' => 'bn_BD', 'direction' => 'ltr'],
+        'PK' => ['name' => 'Pakistan', 'currency' => 'PKR', 'locale' => 'en_PK', 'direction' => 'ltr'],
+        'IN' => ['name' => 'India', 'currency' => 'INR', 'locale' => 'en_IN', 'direction' => 'ltr'],
+    ],
+];
