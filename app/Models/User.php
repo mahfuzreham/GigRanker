1b9e67454c95677e40555da7d6ee4b665a41b057
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2026-08-16 07:05:45 +0600

    fix: enable user model factories

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasFactory, Notifiable;
 
     protected $fillable = [
         'name',
