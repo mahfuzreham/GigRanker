9764c6db5f346c283c150088811f89ee812eeb77
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2026-08-16 06:51:16 +0600

    fix: use Laravel application bootstrap trait in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    public function createApplication(): Application
-    {
-        return require __DIR__.'/../bootstrap/app.php';
-    }
+    use CreatesApplication;
 }
